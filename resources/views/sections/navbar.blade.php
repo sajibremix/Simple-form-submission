e3d9a769d43c384266f6346656df9990cc09be45
Jakir Hossen <sparkle@Jakirs-Mac-mini.local>
2019-06-20 02:30:04 +0600

    done

--- resources/views/sections/navbar.blade.php
+++ resources/views/sections/navbar.blade.php
@@ -1,10 +1,10 @@
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="/">{{ env('APP_NAME') }}</a>
+      <a class="navbar-brand" href="{{ env('APP_URL') }}">{{ env('APP_NAME') }}</a>
     </div>
     <ul class="nav navbar-nav">
-      <li class="{{ Request::is('/') ? 'active' : '' }}"><a href="/">Home</a></li>
+      <li class="{{ Request::is('/') ? 'active' : '' }}"><a href="{{ env('APP_URL') }}">Home</a></li>
       <li class="{{ Request::is('new-form') ? 'active' : '' }}"><a href="{{ route('newform') }}">Form</a></li>
       <li class="{{ Request::is('forms') ? 'active' : '' }}"><a href="{{ route('forms.index') }}">All submission</a></li>
     </ul>
